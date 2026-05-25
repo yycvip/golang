b3b67a9f513ff9c7634d1e83b0370d19f603e010
Beginners <110331334+yycvip@users.noreply.github.com>
2026-05-25 12:12:31 +0000

    2026.5.25.1

--- 2026/05/5.25.1/main.go
+++ 2026/05/5.25.1/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("继续。。。。。")
+	fmt.Println("hahaha")
 }
--- 2026/05/5.5.1/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("坚持，坚持，坚持！开发一个网站。")
-}
